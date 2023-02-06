7322ce66fc6ce7548c0c3b5844561171a90b3da9
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-02-07 02:51:27 +0300

    setting up database setup and prisma configuration

---
+++ prisma/seed.ts
@@ -0,0 +1,79 @@
+import { db } from '../src/utils/db.server';
+
+type Author = {
+  firstName: string;
+  lastName: string;
+};
+
+type Book = {
+  title: string;
+  isFiction: boolean;
+  datePublished: Date;
+};
+
+async function seed() {
+  await Promise.all(
+    getAuthors().map((author) => {
+      return db.author.create({
+        data: {
+          firstName: author.firstName,
+          lastName: author.lastName,
+        },
+      });
+    })
+  );
+  const author = await db.author.findFirst({
+    where: {
+      firstName: 'can',
+    },
+  });
+
+  await Promise.all(
+    getBooks().map((book) => {
+      const { title, isFiction, datePublished } = book;
+      return db.book.create({
+        data: {
+          title,
+          isFiction,
+          datePublished,
+          authorId: author!.id,
+        },
+      });
+    })
+  );
+}
+
+seed();
+
+function getAuthors(): Array<Author> {
+  return [
+    {
+      firstName: 'can',
+      lastName: 'pelek',
+    },
+    {
+      firstName: 'can',
+      lastName: 'çevik',
+    },
+  ];
+}
+
+function getBooks(): Array<Book> {
+  return [
+    {
+      title: 'sapiens',
+      isFiction: false,
+      datePublished: new Date(),
+    },
+    {
+      title: 'robin hood',
+      isFiction: false,
+      datePublished: new Date(),
+    },
+    {
+      title: 'The wild animals',
+      isFiction: false,
+      datePublished: new Date(),
+    },
+  ];
+}
